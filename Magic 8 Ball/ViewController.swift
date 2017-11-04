96434bf7a64ceb568d701276e57ceb2e57fb11a7
Mirko Cukich <mirkocukich@gmail.com>
2017-11-03 23:46:16 -0500

    Added: ballArray. randomBallNumber, imageView.image

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,26 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Ball Array that holds the 5 ball images w answers
+    let ballArray = ["ball1","ball2","ball3","ball4","ball5"]
+    // Radom number variable
+    var randomBallNumber : Int = 0
+    
     @IBOutlet weak var imageView: UIImageView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Generates random number
+        randomBallNumber = Int(arc4random_uniform(4))
+        // Based on random number generated, images switch on device
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
     @IBAction func askBtn(_ sender: Any) {
         
         
     }
     
-    
-
 }
 
